e1d2760568b45186f4991ef7ce092ec5f1a4bbf8
fabio favaretto <fabio.favaretto@upduo.com.br>
2022-05-01 09:05:38 -0300

    git init

---
+++ 12.1.33+/managerRPO_rpo_custom/managerRPO.bat
@@ -0,0 +1,2 @@
+powershell -file "E:\TOTVS12\Microsiga\Protheus\managerRPO_rpo_custom\managerRPO.ps1"
+pause
---
+++ 12.1.33+/managerRPO_rpo_padrao/managerRPO.bat
@@ -0,0 +1,2 @@
+powershell -file "E:\TOTVS12\Microsiga\Protheus\managerRPO_rpo_padrao\managerRPO.ps1"
+pause
